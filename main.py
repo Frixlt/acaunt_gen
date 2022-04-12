8eaa57ea18a3d9a336dede13e363d602eb390ce6
ll_BOP_ll <57772236+llBOPll@users.noreply.github.com>
2022-04-12 22:22:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ acaunts={}
 need_pas=int(input("сколько нужно акаунтов:"))
 for i in range(need_pas):
 	lost+=1
-	acaunts[lost]=pas()
+	acaunts[lost]=pas()+pas(1,'1234567890')
 with open('a_gen.txt', 'w+') as file:
 	file.write(str(lost)+"\n")
 	for key, value in acaunts.items():
