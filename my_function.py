8edee9315cf04bfacdd2f7e8f0dc896e14fd3948
ll_BOP_ll <05mba0622@gmail.com>
2022-04-13 22:00:15 +0300

    small improvements

--- my_function.py
+++ my_function.py
@@ -6,6 +6,7 @@ def pas(long=8,dict=False):
 	for i in range(long):
 		new_pas+=random.choice(list(dict))
 	return new_pas
+
 def max(f):
     with open(f, 'r') as file:
         lines = 0
--- start_site.py
+++ start_site.py
@@ -2,20 +2,22 @@ from flask import Flask, request, session, render_template
 from my_function import max
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'VERY_SECRET_KEY'
-email = "your_email"
+
+email = "your_email" # Ваша почта, будет отображаться на сайте к каждому акаунту
+online = True #открыть сервер для сети? (True/False)
 
 @app.route('/', methods=['POST', 'GET'])
 def start():
-    session['page'] = 0
-    session['log_and_pas']=['test_login','test_password']
-    return '<meta http-equiv="refresh" content="0;URL=/list" />'
+    session['page'] = 0 #установление значения страницы для нового пользователя
+    session['log_and_pas']=['test_login','test_password'] #установление значения последнего логина и пароля с последующим выводом пользователю
+    return render_template('redirect.html',link = "/list")
 
 @app.route('/list', methods=['POST', 'GET'])
 def list():
     if request.method == 'POST':
         session['page'] += 1
         if max_leng <= session['page']:
-            return '<meta http-equiv="refresh" content="0;URL=/end" />'
+            return render_template('redirect.html',link = "/end")
         with open('a_gen.txt', 'r') as file:
         	session['log_and_pas']=file.readlines()[session['page']][:-1].split(":")
     return render_template('bd.html',email=email, log=(session.get('log_and_pas'))[0], pas=(session.get('log_and_pas'))[1])
@@ -23,9 +25,9 @@ def list():
 @app.route('/end', methods=['POST', 'GET'])
 def end():
 	if request.method == 'POST':
-		return '<meta http-equiv="refresh" content="0;URL=/" />'
+		return render_template('redirect.html',link = "/")
 	return render_template('end.html')
 
 if __name__ == "__main__":
     max_leng = max('a_gen.txt')
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=5000) if online else app.run()
